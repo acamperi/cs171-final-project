178a423748fc7d4b8e22b28b3b89ec4a84d146b8
Dennis Sun <dennissun@college.harvard.edu>
2014-04-08 15:43:34 -0400

    hackish map zoom & click/drag functionalities added

--- main.js
+++ main.js
@@ -30,7 +30,15 @@ var bbDetailVis = {
 	h: 600 - detailVisMargin.top - detailVisMargin.bottom
 };
 
-var centered;
+// ===============================
+//   SETUP FUNCTIONS & VARIABLES
+// ===============================
+
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 9])
+    .on("zoom", move);
+
+var tooltip = d3.select("#mainVis").append("div").attr("class", "tooltip hidden");
 
 // ==============================
 //   CANVAS AND VISFRAMES SETUP
@@ -44,6 +52,8 @@ var mainVisFrame = d3.select("#mainVis")
 		.attr("transform", "translate(" + mainVisMargin.left + "," + mainVisMargin.top + ")")
 	.append("g")
 		.attr("transform", "translate(" + bbMainVis.x + "," + bbMainVis.y + ")")
+	.call(zoom) // applies zoom functionality to the mainVisFrame
+	.on("click", click); // applies on-click zoom functionality
 
 var detailVisFrames = d3.selectAll(".detailVis")
 	.append("svg")
@@ -77,46 +87,66 @@ var loadMap = function() {
 	d3.json("../data/us-named.json", function(error, data) {
 	    var usMap = topojson.feature(data,data.objects.states).features;
 
-    	mainVisFrame.selectAll(".states")
-	        .data(usMap)
-	        .enter()
+		// make map here, for now just draw without setting color for states
+    	var states = mainVisFrame.selectAll(".state")
+	        .data(usMap);
+
+	    states.enter()
 	        .append("path")
 	        .attr("d", path)
-	        .on("click", clicked);
-	    // make map here, for now just draw without setting color for states
+	        .attr("title", function(d,i) { return d.properties.name; });
 
-	    console.log("data");
+	    // offsets for tooltips, from http://techslides.com/demos/d3/worldmap-template.html
+		var offsetL = document.getElementById('mainVis').offsetLeft+20;
+		var offsetT = document.getElementById('mainVis').offsetTop+10;
 
-	    // load in actual data
-	    loadStateData();
+		// tooltips, from http://techslides.com/demos/d3/worldmap-template.html
+		states.on("mousemove", function(d,i) {
+			var mouse = d3.mouse(mainVisFrame.node()).map( function(d) { return parseInt(d); } );
+			tooltip.classed("hidden", false)
+		        .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+		        .html(d.properties.name);
+		  	})
+		  	.on("mouseout",  function(d,i) {
+		    	tooltip.classed("hidden", true);
+		  	}); 
+
+		// load in actual data
+		loadStateData();
 	});
 };
 
-// geometric zoom
-// "borrowed" (modified from) http://bl.ocks.org/mbostock/2206590
-function clicked(d) {
-  var x, y, k;
+// ===============================
+//   MAP INTERACTIVITY FUNCTIONS
+// ===============================
 
-  if (d && centered !== d) {
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    k = 4;
-    centered = d;
-  } else {
-    x = bbMainVis.w / 2;
-    y = bbMainVis.h / 2;
-    k = 1;
-    centered = null;
-  }
+// click & drag functionality, from http://techslides.com/demos/d3/worldmap-template.html
+function move() {
+  var t = d3.event.translate;
+  var s = d3.event.scale; 
+  zscale = s;
+  var h = bbMainVis.h/4;
 
-  mainVisFrame.selectAll("path")
-      .classed("active", centered && function(d) { return d === centered; });
+  t[0] = Math.min(
+    (bbMainVis.w/bbMainVis.h)  * (s - 1), 
+    Math.max(bbMainVis.w * (1 - s), t[0] )
+  );
+  t[1] = Math.min(
+    h * (s - 1) + h * s, 
+    Math.max(bbMainVis.h  * (1 - s) - h * s, t[1])
+  );
 
-  mainVisFrame.transition()
-      .duration(750)
-      .attr("transform", "translate(" + bbMainVis.w / 2 + "," + bbMainVis.h / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      .style("stroke-width", 1.5 / k + "px");
+  zoom.translate(t);
+  mainVisFrame.attr("transform", "translate(" + t + ")scale(" + s + ")");
+
+  //adjust the country hover stroke width based on zoom level
+  d3.selectAll(".state").style("stroke-width", 1.5 / s);
+}
+
+//geo translation on mouse click in map
+function click() {
+  var latlon = projection.invert(d3.mouse(this));
+  console.log(latlon);
 }
 
 // ===============================
