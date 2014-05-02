b91ea7effe812d4f8334ad9e09e6761d34680cd3
Elisabeth Dewailly <edewailly@college.harvard.edu>
2014-05-01 20:00:41 -0400

    added state detail vis (slightly buggy) and made zoom work on dblclick

--- main.js
+++ main.js
@@ -83,6 +83,7 @@ var crimeBarVis = {
 
 var school_dot_radius = 1;
 var zscale = 1;
+var statetab = 0;
 
 // ===============================
 //   SETUP FUNCTIONS & VARIABLES
@@ -90,8 +91,10 @@ var zscale = 1;
 
 // for zoom functionality
 var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
-    // .on("zoom", move);
+    .scaleExtent([1, 10]);
+    //.on("zoom", move);
+
+// console.log(d3.event.sourceEvent.pageX);
 
 // for tooltip functionality
 var tooltip = d3.select("#mainVis").append("div").attr("class", "tooltip");
@@ -144,7 +147,9 @@ var toggleSelected = function(id) {
 	// change map visualization to reflect selected metric
 };
 
-var loadStateAndInstitutionData = function() {
+
+
+var loadStateData = function() {
 	d3.json("data/statesData101112.json", function(error, statesData) {
 		var collegeCrimeScale = d3.scale.linear()
 			.interpolate(d3.interpolateRgb)
@@ -158,6 +163,29 @@ var loadStateAndInstitutionData = function() {
 				return collegeCrimeScale(total);
 			});
 
+		//load data for state average of university detail vis, set selected state name and selected object as the variable for school name and school object to maintain the same configuration 
+		selectedSchool = selectedState;
+		selectedSchoolObject = statesData[selectedState];
+		statetab = 1;
+
+		newPage = true;
+
+		if (newPage === true) {
+					d3.select("#clickPlease")
+						.remove();
+					newPage = false;
+					// tells tabbify to set appropriate tabs for state level
+					tabbify();
+		}
+
+		statedetailify();
+ 		detailified = true;
+		
+ 	});
+ };
+
+			
+var loadInstitutionData = function() {
 		d3.json("data/institutionsData101112.json", function(error, data) {
 			var projections = [];
 			var schoolIDList = [];
@@ -188,11 +216,15 @@ var loadStateAndInstitutionData = function() {
 			.on("click", function(x){
 				selectedSchool = x;
 				selectedSchoolObject = data[x];
+				statetab = 0;
+
+				newPage = true;
 
 				if (newPage === true) {
 					d3.select("#clickPlease")
 						.remove();
 					newPage = false;
+					// tells tabbify to set appropriate tabs for institution level
 					tabbify();
 				}
 
@@ -210,9 +242,10 @@ var loadStateAndInstitutionData = function() {
 			// console.log(selectedSchoolObject);
 			
 			detailfied = true;
+		
 		});
-	});
 };
+
 
 var loadMap = function() {
 	d3.json("data/us-named.json", function(error, data) {
@@ -229,7 +262,12 @@ var loadMap = function() {
 
 	    states.attr("class", "state")
 	    	.attr("stroke", "white")
-	    	.on("click", clicked);
+	    	.on("dblclick", clicked)
+	    	.on("click", function(d, i){
+	    		selectedState = d.properties.code;
+	    		loadStateData();
+	    	});
+	    	
 
 	 //    // offsets for tooltips, from http://techslides.com/demos/d3/worldmap-template.html
 		// var offsetL = document.getElementById('mainVis').offsetLeft+20;
@@ -247,7 +285,7 @@ var loadMap = function() {
 		//   	}); 
 
 		// load in actual data
-		loadStateAndInstitutionData();
+		loadInstitutionData();
 	});
 };
 
@@ -258,9 +296,10 @@ var loadMap = function() {
 // click & drag functionality, from http://techslides.com/demos/d3/worldmap-template.html
 function move() {
 	var t = d3.event.translate;
-	var s = d3.event.scale; 
-	zscale = s;
+	var s = d3.event.scale;
 	var h = bbMainVis.h/4;
+
+	zscale = s;
 
 	t[0] = Math.min(
 		(bbMainVis.w/bbMainVis.h)  * (s - 1), 
@@ -280,10 +319,11 @@ function move() {
 	d3.selectAll(".thisSchool").attr("r", school_dot_radius / s);
 }
 
+
 //geo translation on mouse click in map
 function click() {
   var latlon = projection.invert(d3.mouse(this));
-  // console.log(latlon);
+  console.log(latlon);
 }
 
 function clicked(d) {
@@ -303,8 +343,8 @@ function clicked(d) {
 		centered = null;
 	}
 
-	if (zscale != 1)
-		k = 4;
+	// if (zscale != 1)
+	// 	k = 4;
 
 	zoom.scale(k);
 	zoom.translate([x, y]);
@@ -320,7 +360,8 @@ function clicked(d) {
   
   	d3.selectAll(".state").transition().duration(750).style("stroke-width", 1 / k);
   	d3.selectAll(".school").transition().duration(750).attr("r", school_dot_radius / k);
-  	d3.selectAll(".thisSchool").transition().duration(750).attr("r", school_dot_radius / k);
+		d3.selectAll(".thisSchool").transition().duration(750).attr("r", school_dot_radius / k);
+
 }
 
 // ===============================
@@ -397,6 +438,37 @@ function searchForImageForCollege(college_name) {
 
 // function that creates the SVG tabs
 function tabbify() {
+
+// // remove previous tabs
+// if (tabbify === true) {
+// 		// selects visualization
+// 		vis1 = d3.select("#detailVis");
+
+// 		// removes school header
+// 		vis1.select("h2")
+// 			.remove();
+
+// 		// removes table
+// 		vis1.select("#dataTable")
+// 			.remove();
+
+// 		// removes everything from the canvas
+// 		svg1 = vis1.select("#canvas");
+// 		svg1.selectAll("g")
+// 			.remove();
+// 		svg1.selectAll("text")
+// 			.remove();
+
+// 		vis2 = d3.select("#detailVis2");
+
+// 		// removes everything from the SVG
+// 		svg2 = vis2.select("svg");
+// 		svg2.selectAll("g")
+// 			.remove();
+
+// }
+
+
 	var tabBar = d3.select("#detailVis")
 		.insert("svg", "#canvas")
 			.attr("width", bbDetailTabs.w)
@@ -416,7 +488,13 @@ function tabbify() {
 		.attr("text-anchor", "middle")
 		.attr("x", bbDetailTabs.w/(bbDetailTabs.barCount*2))
 		.attr("y", bbDetailTabs.h/2 + 5)
-		.text("School Data");
+		.text( function () { 
+			if (statetab == 0){
+				return "School Data";
+			}
+			else
+				return "State Data"; 
+		});
 
 	var tabBar2 = tabBar.append("g")
 		.attr("id", "tabBar2");
@@ -432,19 +510,23 @@ function tabbify() {
 		.attr("y", bbDetailTabs.h/2 + 5)
 		.text("Crime Statistics");
 
-	var tabBar3 = tabBar.append("g")
-		.attr("id", "tabBar3");
-	tabBar3.append("rect")
-		.attr("x", (bbDetailTabs.w/bbDetailTabs.barCount) * 2)
-		.attr("y", 0)
-		.attr("width", bbDetailTabs.w/bbDetailTabs.barCount)
-		.attr("height", bbDetailTabs.h)
-		.attr("fill", "#BBBBBB");
-	tabBar3.append("text")
-		.attr("text-anchor", "middle")
-		.attr("x", (bbDetailTabs.w/bbDetailTabs.barCount) * 2 + bbDetailTabs.w/(bbDetailTabs.barCount*2))
-		.attr("y", bbDetailTabs.h/2 + 5)
-		.text("School Comparison");
+	if (statetab == 0) {
+
+		var tabBar3 = tabBar.append("g")
+			.attr("id", "tabBar3");
+		tabBar3.append("rect")
+			.attr("x", (bbDetailTabs.w/bbDetailTabs.barCount) * 2)
+			.attr("y", 0)
+			.attr("width", bbDetailTabs.w/bbDetailTabs.barCount)
+			.attr("height", bbDetailTabs.h)
+			.attr("fill", "#BBBBBB");
+		tabBar3.append("text")
+			.attr("text-anchor", "middle")
+			.attr("x", (bbDetailTabs.w/bbDetailTabs.barCount) * 2 + bbDetailTabs.w/(bbDetailTabs.barCount*2))
+			.attr("y", bbDetailTabs.h/2 + 5)
+			.text("School Comparison");
+
+	}
 
 	tabBar1.on("click", function(){
 		if(currentTab != 1) {
@@ -496,30 +578,32 @@ function tabbify() {
 		}
 	});
 
-	tabBar3.on("click", function(){
-		if(currentTab != 3) {
-			d3.selectAll(".tab1")
-				.attr("opacity", "0");
-			d3.selectAll(".tab2")
-				.attr("opacity", "0");
-			d3.selectAll(".tab3")
-				.attr("opacity", "1");
+	if (statetab == 0) {
+		tabBar3.on("click", function(){
+			if(currentTab != 3) {
+				d3.selectAll(".tab1")
+					.attr("opacity", "0");
+				d3.selectAll(".tab2")
+					.attr("opacity", "0");
+				d3.selectAll(".tab3")
+					.attr("opacity", "1");
 
-			d3.select("#tabBar1")
-				.select("svg rect")
-				.attr("fill", "#BBBBBB");
-			d3.select("#tabBar2")
-				.select("svg rect")
-				.attr("fill", "#BBBBBB");
-			d3.select("#tabBar3")
-				.select("svg rect")
-				.attr("fill", "#FFFFFF");
+				d3.select("#tabBar1")
+					.select("svg rect")
+					.attr("fill", "#BBBBBB");
+				d3.select("#tabBar2")
+					.select("svg rect")
+					.attr("fill", "#BBBBBB");
+				d3.select("#tabBar3")
+					.select("svg rect")
+					.attr("fill", "#FFFFFF");
 
-			d3.select("#dataTable").style("opacity", "0");
+				d3.select("#dataTable").style("opacity", "0");
 
-			currentTab = 3;
-		}
-	});
+				currentTab = 3;
+			}
+		});
+	}
 }
 
 // function that generates all the detail visualizations
@@ -557,6 +641,43 @@ function detailify() {
 	pieBaker();
 	financify();
 	crimeify();
+	detailified = true;
+}
+
+// function that generates all the detail visualizations
+function statedetailify() {
+	if (detailified === true) {
+		// selects visualization
+		vis1 = d3.select("#detailVis");
+
+		// removes school header
+		vis1.select("h2")
+			.remove();
+
+		// removes table
+		vis1.select("#dataTable")
+			.remove();
+
+		// removes everything from the canvas
+		svg1 = vis1.select("#canvas");
+		svg1.selectAll("g")
+			.remove();
+		svg1.selectAll("text")
+			.remove();
+
+		vis2 = d3.select("#detailVis2");
+
+		// removes everything from the SVG
+		svg2 = vis2.select("svg");
+		svg2.selectAll("g")
+			.remove();
+
+	}
+	detailified = false;
+	genderize();
+	pieBaker();
+	financify();
+	statecrimeify();
 	detailified = true;
 }
 
@@ -1048,3 +1169,120 @@ function crimeify() {
     }   
 }
 
+function statecrimeify() {
+
+	crimeInfoBuffer = {};
+	crimeInfoBuffer["total"] = parseInt(selectedSchoolObject["college_crime"]["total"]);
+	crimeInfoBuffer["murder"] = parseInt(selectedSchoolObject["college_crime"]["murder"]);
+	crimeInfoBuffer["negligent manslaughter"] = parseInt(selectedSchoolObject["college_crime"]["negligent_manslaughter"]);
+	crimeInfoBuffer["forcible sex offense"] = parseInt(selectedSchoolObject["college_crime"]["forcible_sex_offense"]);
+	crimeInfoBuffer["nonforcible sex offense"] = parseInt(selectedSchoolObject["college_crime"]["nonforcible_sex_offense"]);
+	crimeInfoBuffer["robbery"] = parseInt(selectedSchoolObject["college_crime"]["robbery"]);
+	crimeInfoBuffer["aggravated assault"] = parseInt(selectedSchoolObject["college_crime"]["aggravated_assault"]);
+	crimeInfoBuffer["burglary"] = parseInt(selectedSchoolObject["college_crime"]["burglary"]);
+	crimeInfoBuffer["motor vehicle theft"] = parseInt(selectedSchoolObject["college_crime"]["motor_vehicle_theft"]);
+	crimeInfoBuffer["arson"] = parseInt(selectedSchoolObject["college_crime"]["arson"]);
+
+	crimeInfo = d3.entries(crimeInfoBuffer);
+	crimeInfoKeys = d3.keys(crimeInfoBuffer);
+
+	// console.log(crimeInfo);
+
+	xScale = d3.scale.ordinal()
+		.domain(crimeInfoKeys)
+		.rangePoints([0, crimeBarVis.w]);  // define the right domain generically
+    yScale = d3.scale.linear()
+    	.domain([0, d3.max(crimeInfo, function(d){
+    		return d.value;
+   		})])
+   		.range([0, crimeBarVis.h]);
+   	inverseYScale = d3.scale.linear()
+    	.domain([0, d3.max(crimeInfo, function(d){
+    		return d.value;
+   		})])
+   		.range([crimeBarVis.h, 0]);
+
+	var cBXAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom");
+
+    var cBYAxis = d3.svg.axis()
+      .scale(inverseYScale)
+      .orient("left");
+
+	// selects the canvas on which to make the visualization
+    var crimeBars = d3.select("#detailVis")
+    	.select("#canvas")
+    	.append("g")
+    	.attr("id", "crimeBars")
+    	.attr("class", "tab2");
+
+    // makes the title
+    crimeBars.append("text")
+		.attr("class", "detailVisHeader")
+		.attr("x", crimeBarVis.x)
+		.attr("y", (crimeBarVis.y - 5))
+		.text("University Crime");
+
+	// makes the bars
+	crimeBars.selectAll(".bar")
+        .data(crimeInfo)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d, i) { 
+        	return crimeBarVis.barWidth/2 + crimeBarVis.chartX + xScale(d.key);
+        })
+        .attr("y", function(d) {
+        	return crimeBarVis.chartY + (crimeBarVis.h - yScale(d.value));
+        })
+        .attr("width", function(d, i) {
+        	return crimeBarVis.barWidth;
+        })
+        .attr("height", function(d) {
+        	return yScale(d.value);
+        })
+        .attr("title", function(d) {
+        	return d.key;
+        });
+
+    crimeBars.append("g")
+        .attr("class", "axis")
+        .attr("id", "crimeXAxis")
+        .attr("transform", "translate(" + (crimeBarVis.chartX + crimeBarVis.barWidth) + "," + crimeBarVis.xAxisY +")")
+        .call(cBXAxis)
+      	// modified from http://www.d3noob.org/2013/01/how-to-rotate-text-labels-for-x-axis-of.html
+        	.selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-65)" ;
+            });
+
+    crimeBars.append("g")
+        .attr("class", "axis")
+        .attr("id", "crimeYAxis")
+        .attr("transform", "translate(" + crimeBarVis.chartX + "," + crimeBarVis.chartY +")")
+        .call(cBYAxis);
+
+    if (crimeInfoBuffer["total"] == 0) {
+    	var blankAlert = crimeBars.append("g")
+    		.attr("class", "alertWindow");
+    	blankAlert.append("rect")
+    		.attr("width", "120px")
+    		.attr("height", "20px")
+    		.attr("x", crimeBarVis.w/2 - 20)
+    		.attr("y", crimeBarVis.h/2 + 15);
+    	blankAlert.append("text")
+    		.attr("x", crimeBarVis.w/2 + 40)
+    		.attr("y", crimeBarVis.h/2 + 30)
+    		.attr("text-anchor", "middle")
+    		.text("No data available.");
+    }
+
+    if (currentTab != 2) {
+    	d3.selectAll(".tab2")
+    	.attr("opacity", "0");
+    }   
+}
